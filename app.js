8c40086964949e0d58ea9179b8303d348815b25c
Ebere Onuobia <cynthiaonuobia@gmail.com>
2023-09-23 19:22:10 +0100

    rrr

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const cors = require('cors');
 const routes = require('./routes/routes');
 const db = require('./config/database');
-
+ 
 const corsOptions = {
   origin: ['http://localhost:5174', 'http://localhost:5173', 'http://localhost:5175'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
